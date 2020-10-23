385da1325624fa0153b9c41035891fe809e44379
David Gond <david.gond.coding@gmail.com>
2020-10-23 17:59:00 +0200

    feat(project): State of the project after session 3

--- characters/enemies/enemy.gd
+++ characters/enemies/enemy.gd
@@ -4,13 +4,24 @@ func _ready() -> void:
 	set_physics_process(false)
 	_velocity.x = -speed.x
 
+
 func _physics_process(delta: float) -> void:
 	_velocity = manage_velocity(_velocity)
 	if is_on_wall():
 		_velocity.x *= -1.0
 	_velocity.y = move_and_slide(_velocity, FLOOR_NORMAL).y
-	
+
+
 func manage_velocity(linear_velocity: Vector2) -> Vector2:
 	var out: = linear_velocity
 	out.y += gravity * get_physics_process_delta_time()
 	return out
+
+
+func _on_StompDetector_body_entered(body: PhysicsBody2D) -> void:
+	if body.global_position.y > get_node("StompDetector").global_position.y:
+		return
+	# On désactive les collisions
+	get_node("CollisionShape2D").disabled = true
+	# On tue l'ennemi
+	queue_free()
--- characters/players/player.gd
+++ characters/players/player.gd
@@ -1,5 +1,7 @@
 extends Actor
 
+export var stomp_impulse: = 600.0
+
 func _physics_process(delta: float) -> void:
 	var direction: = get_direction()
 	
@@ -8,7 +10,8 @@ func _physics_process(delta: float) -> void:
 	
 	_velocity = manage_velocity(_velocity, direction, is_jump_interrupted)
 	_velocity = move_and_slide(_velocity, FLOOR_NORMAL)
-	
+
+
 func manage_velocity(linear_velocity: Vector2, direction: Vector2, is_jump_interrupted: bool) -> Vector2:
 	var out: = linear_velocity
 	out.x = direction.x * speed.x
@@ -20,6 +23,17 @@ func manage_velocity(linear_velocity: Vector2, direction: Vector2, is_jump_inter
 		out.y = 0.0
 	return out
 
+
 func get_direction() -> Vector2:
 	return Vector2(Input.get_action_strength("move_right") - Input.get_action_strength("move_left"), 
 	-1.0 if Input.is_action_just_pressed("move_jump") and is_on_floor() else 0.0)
+
+
+func _on_StompDetector_area_entered(area: Area2D) -> void:
+	# On force la vélocité verticale pour donner l'impulsion
+	_velocity.y = -stomp_impulse
+
+
+func _on_Enemy_detector_body_entered(body: PhysicsBody2D) -> void:
+	# On tue le joueur
+	queue_free()
